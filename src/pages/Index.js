a403fa9706a5ef47efe87e30e8b65b9185d88eda
raeoxfordhebron <raeoxfordhebron@gmail.com>
2023-02-20 12:15:50 -0500

    index functions

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,15 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 const Index = (props) => {
-    return <h1>Index</h1>;
+  const jobs = useLoaderData()  
+  return jobs.map(job => (
+    <div key={job.id} className="job">
+      <Link to={`/${job.id}`}>
+        <h1>{job.title}</h1>
+      </Link>
+      <h3>{job.description}</h3>
+    </div>
+  ));
   };
   
   export default Index;
