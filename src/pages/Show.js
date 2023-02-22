aac72eeb83f033f16dd414a86d91a588cc46fce3
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-21 20:02:15 -0500

    show is fully functional

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,17 @@
+import { Link, useLoaderData } from "react-router-dom";
+
 const Show = (props) => {
-    return <h1>Show</h1>;
+  const job = useLoaderData()  
+  return (
+    <div>
+      <h1>{job.title}</h1>
+      <h2>{job.type}</h2>
+      <p>{job.description}</p>
+      <Link to="/">
+        <button>Go Back</button>
+      </Link>
+    </div>
+  );
   };
   
   export default Show;
