15e0db02ffeb662d2c5aa7f49821123de7c3e1c3
raeoxfordhebron <rae.oxford.hebron@gmail.com>
2023-02-22 20:53:13 -0500

    search bar functionality

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,15 +1,39 @@
 import { Link, useLoaderData } from "react-router-dom";
+import { useState, useRef } from "react";
 
 const Index = (props) => {
-  const jobs = useLoaderData()  
-  return jobs.map(job => (
+  const jobs = useLoaderData()
+  const [displayedJobs, setDisplayedJobs] = useState(jobs)
+  const inputRef = useRef(null)
+  const searchJobs = (searchString) => {
+    return jobs.filter((job) => {
+      return job.title.toLowerCase().includes(searchString.toLowerCase())
+    })
+  }  
+
+  const handleSubmit = (event) => {
+    const search = inputRef.current.value
+    if(search === ""){
+      setDisplayedJobs(jobs)
+      return 1
+    }
+    setDisplayedJobs(searchJobs(search))
+  }
+  return (<>
+    Search: <input type="text" ref={inputRef}/> <input onClick={handleSubmit} type="submit"/>
+    <div className="job-container">
+    {displayedJobs.map(job => (
     <div key={job.id} className="job">
       <Link to={`/${job.id}`}>
         <h1>{job.title}</h1>
       </Link>
       <h3>{job.description}</h3>
     </div>
-  ));
+  ))}
+  </div>
+  </>
+  );
+
   };
   
   export default Index;
