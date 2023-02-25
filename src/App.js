5ccd08104b26f1c1e42261242846a73688ec18c7
raeoxfordhebron <rae.oxford.hebron@gmail.com>
2023-02-25 16:25:34 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Sidenav from './components/Sidenav';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Sidenav/>
       <Outlet/>
     </div>
   );
