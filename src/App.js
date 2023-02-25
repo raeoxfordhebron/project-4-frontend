249e92fbe8e262d4f18307db19e4240623ff9df9
raeoxfordhebron <rae.oxford.hebron@gmail.com>
2023-02-25 11:00:19 -0500

    tweaking layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Sidenav from './components/Sidenav';
 
 function App() {
   return (
